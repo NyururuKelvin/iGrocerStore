2f0ca0154a7a3039832d685a401842c879c5ee64
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-06 21:09:49 +0300

    add toggle function

--- js-herbs/herbs.js
+++ js-herbs/herbs.js
@@ -13,6 +13,15 @@ $(document).ready(function(){
         $(this).find('.add-to-cart').hide('slow');
     })
     $('.add-to-cart').click(function(){
-        swal("Item added to cart", "click anywhere on the page to continue", "success");
+        swal("Item added to cart", "Continue Shopping", "success");
     })
 })
+
+(function(){
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
+
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    })
+})();
